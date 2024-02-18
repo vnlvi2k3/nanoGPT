f27f244dc7c14748b4210b19c41a5721d66165aa
LAPTOP-IGLVDQP8\Lan Vi <vi.vuvivu2203@hcmut.edu.vn>
2024-02-18 19:32:22 +0700

    update

--- gpt.py
+++ gpt.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import tqdm
+from tqdm import tqdm
 
 #hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
